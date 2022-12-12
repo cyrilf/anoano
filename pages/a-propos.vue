eabdafb0ae16033bdef37c8db486eef1dcb1a7df
cyrilf <1302282+cyrilf@users.noreply.github.com>
2022-12-12 17:53:24 +0100

    feat(content): Fill the A propos page; Close #11

--- pages/a-propos.vue
+++ pages/a-propos.vue
@@ -1,27 +1,38 @@
 <template>
   <div>
-    <section class="body-font text-gray-600">
-      <div
-        class="container mx-auto flex flex-col items-center px-5 py-24 md:flex-row"
-      >
-        <div
-          class="mb-16 flex flex-col items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24"
-        >
-          <p class="leading-relaxed">
+    <section>
+      <div class="container mx-auto px-5">
+        <div class="prose m-4 mx-auto p-4">
+          <p>
             Ce site souhaite parler de contraception masculine. Principalement
-            de la contraception thermique avec l'anneau.
+            de la <b>contraception thermique</b> avec l'anneau.
           </p>
-          <p class="leading-relaxed">
+          <p>
             Le mieux pour commencer, est de suivre le parcours. C'est un contenu
             en cours de création sur les différentes étapes pour se
             contracepter.
           </p>
           <NuxtLink
             to="/parcours"
-            class="mt-4 inline-flex rounded border-0 bg-yellow-400 py-2 px-6 text-lg text-white hover:bg-yellow-500 focus:outline-none"
+            class="my-4 inline-flex rounded border-0 bg-yellow-400 py-2 px-6 text-lg text-white no-underline hover:bg-yellow-500 focus:outline-none"
           >
             Le parcours
           </NuxtLink>
+          <p>
+            La vocation du site est de partager du
+            <b>contenu clair et détailé</b> ainsi que des
+            <b>outils pratiques</b>. Tout en <b>libre et open source</b> (code,
+            textes, etc.). <icon name="🥳" />
+          </p>
+          <p>
+            <b>Tout le monde est le.la bienvenue pour participer !</b> Et
+            surtout toi, oui, toi ! <icon name="👋️" />
+          </p>
+          <p>
+            C'est encore un peu le chantier, mais si tu veux me contacter, tu
+            peux écrire à
+            <a href="mailto:koeko+anoano@proton.me">koeko+anoano@proton.me</a>
+          </p>
         </div>
       </div>
     </section>
