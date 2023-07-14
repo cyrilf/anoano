365374590a85459c71ff0419d7e679c8017d6c0a
cyrilf <1302282+cyrilf@users.noreply.github.com>
2023-07-14 22:10:45 +0200

    ui: fix the header on mobile by adding a menu
    fix #24

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -1,34 +1,59 @@
 <template>
-  <div class="mb-28 md:mb-16">
+  <div :class="` ${isMenuOpen ? 'mb-40 sm:mb-28' : 'mb-16'} md:mb-16`">
     <header :class="`fixed z-30 w-full bg-white shadow ${headerClass}`">
       <div
         class="container mx-auto flex flex-col flex-wrap items-center px-4 py-3 md:flex-row"
       >
-        <nuxt-link
-          to="/"
-          class="group flex items-center font-medium text-primary-400 transition-colors hover:text-primary-500"
-          active-class="_"
-          exact-active-class="_"
-        >
-          <svg
-            class="h-10 w-10 rounded-full bg-primary-400 stroke-primary-400 transition-colors group-hover:bg-primary-500 group-hover:stroke-primary-500"
+        <div class="flex gap-8">
+          <button class="w-4 md:hidden" @click.prevent="toggleMenu()">
+            <icon name="fe:bar" class="text-xl" />
+          </button>
+          <nuxt-link
+            to="/"
+            class="group flex items-center font-medium text-primary-400 transition-colors hover:text-primary-500"
+            active-class="_"
+            exact-active-class="_"
           >
-            <circle cx="50%" cy="50%" r="50%" stroke-width="40%" fill="white" />
-            <line x1="-10%" y1="70%" x2="70%" y2="-10%" stroke-width="30%" />
-          </svg>
+            <svg
+              class="h-10 w-10 rounded-full bg-primary-400 stroke-primary-400 transition-colors group-hover:bg-primary-500 group-hover:stroke-primary-500"
+            >
+              <circle
+                cx="50%"
+                cy="50%"
+                r="50%"
+                stroke-width="40%"
+                fill="white"
+              />
+              <line x1="-10%" y1="70%" x2="70%" y2="-10%" stroke-width="30%" />
+            </svg>
 
-          <span class="ml-3 text-2xl font-extrabold">Anoano</span>
-        </nuxt-link>
+            <span class="ml-3 text-2xl font-extrabold">Anoano</span>
+          </nuxt-link>
+        </div>
         <nav
-          class="mt-5 flex flex-wrap items-center justify-center gap-y-4 text-base md:ml-auto md:mt-0"
+          :class="`mt-5 flex h-20 flex-wrap items-center justify-center gap-y-4 text-base transition sm:h-auto md:ml-auto md:mt-0 ${
+            isMenuOpen ? 'block' : 'hidden'
+          } md:block`"
         >
-          <nuxt-link to="/guides" class="mr-5 hover:text-primary-500">
+          <nuxt-link
+            to="/guides"
+            class="mr-5 hover:text-primary-500"
+            @click="isMenuOpen && toggleMenu()"
+          >
             Les guides
           </nuxt-link>
-          <nuxt-link to="/outils" class="mr-5 hover:text-primary-500">
+          <nuxt-link
+            to="/outils"
+            class="mr-5 hover:text-primary-500"
+            @click="isMenuOpen && toggleMenu()"
+          >
             Les outils
           </nuxt-link>
-          <nuxt-link to="/extras" class="mr-5 hover:text-primary-500">
+          <nuxt-link
+            to="/extras"
+            class="mr-5 hover:text-primary-500"
+            @click="isMenuOpen && toggleMenu()"
+          >
             Les extras
           </nuxt-link>
           <nuxt-link
@@ -36,6 +61,7 @@
             active-class="_"
             exact-active-class="_"
             class="inline-flex items-center gap-1 rounded border-2 border-accent-400 px-3 py-1 text-base transition hover:bg-accent-400 hover:text-white"
+            @click="isMenuOpen && toggleMenu()"
           >
             <svg
               fill="none"
@@ -58,6 +84,11 @@
 <script setup lang="ts">
 const router = useRouter();
 const headerClass = ref("");
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
 
 watchEffect(() => {
   headerClass.value =
